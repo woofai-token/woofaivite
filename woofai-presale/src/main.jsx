5e9f803031802b557f903b59b14eef9d43b3e6c5
woofai-token <woofai.coin@gmail.com>
2025-05-17 21:30:26 +0530

    Update main.jsx

--- woofai-presale/src/main.jsx
+++ woofai-presale/src/main.jsx
@@ -1,34 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App";
 
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+const network = "https://mainnet.helius-rpc.com/?api-key=fabdaf9f-b1de-4a1b-bb03-58532838cea3";
+
 import {
   PhantomWalletAdapter,
   SolflareWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
 
-import "@solana/wallet-adapter-react-ui/styles.css";
-
-const NETWORK = "https://mainnet.helius-rpc.com/?api-key=fabdaf9f-b1de-4a1b-bb03-58532838cea3";
-const IS_MOBILE = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
 const wallets = [
   new PhantomWalletAdapter(),
   new SolflareWalletAdapter(),
 ];
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <React.StrictMode>
-    <ConnectionProvider endpoint={NETWORK}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <App />
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ConnectionProvider endpoint={network}>
+    <WalletProvider wallets={wallets} autoConnect>
+      <WalletModalProvider>
+        <App />
+      </WalletModalProvider>
+    </WalletProvider>
+  </ConnectionProvider>
 );
